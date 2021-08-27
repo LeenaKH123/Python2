25c8bcda556f52cb59a5f5642cb484cab88e99b5
Leena <engleena@yahoo.com>
2021-08-27 14:11:37 +0300

    section 2

--- 02_more-datatypes/02_12_add_or_not.py
+++ 02_more-datatypes/02_12_add_or_not.py
@@ -5,4 +5,24 @@
 # If the element was already in the set, notify the user that their
 # input hasn't been added and deduce a point.
 # If the user gets loses 5 points, quite the program.
-# They wil if they manage to create a set that has more than 10 items.
+# They will win if they manage to create a set that has more than 10 items.
+
+
+user_set = set({})
+win = False
+points = 0
+while not win:
+    user_input = input("please type a number ")
+    user_input_int = int(user_input)
+    if user_input_int in user_set:
+        points -= 1
+        print("your number is not added and you lost a point")
+        print("you currently have ", points)
+        if points == -5:
+            print("you lost")
+            break
+    else:
+        user_set.add(user_input_int)
+        if len(user_set) > 10:
+            win = True
+            print("You won!")
--- 02_more-datatypes/02_16_inspiring_quotes.py
+++ 02_more-datatypes/02_16_inspiring_quotes.py
@@ -4,15 +4,56 @@
 #
 # "The inspiring quote" - Lastname, Firstname
 
+# name = "Tom"
+# age = 99
+
+# print(f"Hello {name}. Next year you'll be {age + 1}!")
+
 famous_quotes = [
-    {"full_name": "Isaac Asimov", "quote": "I do not fear computers. I fear lack of them."},
-    {"full_name": "Emo Philips", "quote": "A computer once beat me at chess, but it was no match for me at "
-                                          "kick boxing."},
-    {"full_name": "Edsger W. Dijkstra", "quote": "Computer Science is no more about computers than astronomy "
-                                                 "is about telescopes."},
-    {"full_name": "Bill Gates", "quote": "The computer was born to solve problems that did not exist before."},
-    {"full_name": "Norman Augustine", "quote": "Software is like entropy: It is difficult to grasp, weighs nothing, "
-                                               "and obeys the Second Law of Thermodynamics; i.e., it always increases."},
-    {"full_name": "Nathan Myhrvold", "quote": "Software is a gas; it expands to fill its container."},
-    {"full_name": "Alan Bennett", "quote": "Standards are always out of date.  That’s what makes them standards."}
+    {
+        "full_name": "Isaac Asimov",
+        "quote": "I do not fear computers. I fear lack of them.",
+    },
+    {
+        "full_name": "Emo Philips",
+        "quote": "A computer once beat me at chess, but it was no match for me at "
+        "kick boxing.",
+    },
+    {
+        "full_name": "Edsger W. Dijkstra",
+        "quote": "Computer Science is no more about computers than astronomy "
+        "is about telescopes.",
+    },
+    {
+        "full_name": "Bill Gates",
+        "quote": "The computer was born to solve problems that did not exist before.",
+    },
+    {
+        "full_name": "Norman Augustine",
+        "quote": "Software is like entropy: It is difficult to grasp, weighs nothing, "
+        "and obeys the Second Law of Thermodynamics; i.e., it always increases.",
+    },
+    {
+        "full_name": "Nathan Myhrvold",
+        "quote": "Software is a gas; it expands to fill its container.",
+    },
+    {
+        "full_name": "Alan Bennett",
+        "quote": "Standards are always out of date.  That’s what makes them standards.",
+    },
 ]
+
+for dictionary in famous_quotes:
+    quote = dictionary["quote"]
+    full_name = dictionary["full_name"]
+    list_ = full_name.split(" ")
+    if len(list_) == 3:
+        first_name = list_[0] + " " + list_[1]
+        last_name = list_[2]
+    else:
+        first_name = list_[0]
+        last_name = list_[1]
+
+    # "The inspiring quote" - Lastname, Firstname
+    # print(f"Hello {name}. Next year you'll be {age + 1}!")
+    print(f'"{quote}" - {last_name}, {first_name}')
