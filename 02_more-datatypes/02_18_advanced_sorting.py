4e22ba04288cdfeadd7307043f7f68a22fdcde5c
LeenaKH123 <69055679+LeenaKH123@users.noreply.github.com>
2021-08-30 11:02:41 +0300

    Update 02_18_advanced_sorting.py
    list is created from dictionary.

--- 02_more-datatypes/02_18_advanced_sorting.py
+++ 02_more-datatypes/02_18_advanced_sorting.py
@@ -8,3 +8,5 @@
 # even if you don't yet completely understand why it works the way it does:
 # https://docs.python.org/3/howto/sorting.html#key-functions
 # Feel free to discuss any questions you have with your mentor and on the forum!
+input_dict = {"item1": 5, "item2": 6, "item3": 1}
+print(input_dict.items())
