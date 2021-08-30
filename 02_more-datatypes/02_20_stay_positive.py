e9daa4dc75ef12a3354b8c72b511ee3db8f4570e
LeenaKH123 <69055679+LeenaKH123@users.noreply.github.com>
2021-08-30 11:00:23 +0300

    Update 02_20_stay_positive.py
    Positive list created

--- 02_more-datatypes/02_20_stay_positive.py
+++ 02_more-datatypes/02_20_stay_positive.py
@@ -2,3 +2,8 @@
 # `numbers` that contains only the positive numbers from the provided list.
 
 numbers = [5, -8, 3, 10, -19, -22, 44, 2, -1, 4, 42]
+list_ = []
+for num in numbers:
+    if num > 0:
+         list_.append(num)
+print(list_)
