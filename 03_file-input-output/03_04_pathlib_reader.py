99577486295236881e626fcf429b115585f18240
Leena <engleena@yahoo.com>
2021-09-03 17:24:52 +0300

    almost done with section 3

--- 03_file-input-output/03_04_pathlib_reader.py
+++ 03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,7 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+from pathlib import Path
+
+data_path = Path("\\wsl$\Ubuntu\home\leena\python-201\03_file-input-output\file-counts.csv")
+with open(data_path.joinpath)
