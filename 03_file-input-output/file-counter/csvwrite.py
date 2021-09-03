ef5a40d137aa778a517bcab74314e51b31129e3b
Leena <engleena@yahoo.com>
2021-09-03 16:23:22 +0300

    done with file count

--- 03_file-input-output/file-counter/csvwrite.py
+++ 03_file-input-output/file-counter/csvwrite.py
@@ -1,1 +1,8 @@
 # Write the file counts to a `.csv` file.
+# file_out = open("output.txt", "w")
+# file_out.write('This is what you are writing to the file')
+# file_out.close()
+with open("03_file-input-output/file-counter/filecounts.csv") as f:
+    with open("03_file-input-output/file-counter/filecounts.txt", "w") as f1:
+        for line in f:
+            f1.write(line)
