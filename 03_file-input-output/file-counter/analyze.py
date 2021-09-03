60f62798fe49b95c2aacbf4683e65c77114170ca
Leena <engleena@yahoo.com>
2021-09-03 12:41:52 +0300

    try

--- 03_file-input-output/file-counter/analyze.py
+++ 03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,12 @@
 # Use the `csv` module to read in and count the different file types.
+# import CSV
+# With open(‘some.csv’, ‘rb’) as f:
+# reader = csv.reader(f)
+# for row in reader:
+# print row
+import csv
+
+with open("filecounts.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
