ba2f9071426f3f6116959cc7285b698f2f74f9fc
Leena <engleena@yahoo.com>
2021-09-03 16:30:21 +0300

    done with long words

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,6 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+with open("03_file-input-output/words.txt") as f:
+    for row in f:
+        if len(row) > 20:
+            print(row)
