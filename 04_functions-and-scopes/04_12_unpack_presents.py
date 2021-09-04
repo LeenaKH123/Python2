e1e083c759990645278bd6e333f1ece43ed422ba
Leena <engleena@yahoo.com>
2021-09-04 10:15:54 +0300

    passing dictionary to a function

--- 04_functions-and-scopes/04_12_unpack_presents.py
+++ 04_functions-and-scopes/04_12_unpack_presents.py
@@ -3,7 +3,10 @@
 # You shouldn't explicitly access the dict values, but use
 # dictionary unpacking when passing the argument instead.
 
+
 def congratulate(name, age):
     return f"Today {name} is {age} years old.\nHappy Birthday!"
 
+
 user = {"name": "Adelheid", "age": 22}
+print(congratulate(*user))
