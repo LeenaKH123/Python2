d7a6053de6beaff8abcc2a71d8936d8ffaa26bbe
root <root@DESKTOP-G07NF43.localdomain>
2021-09-14 09:38:12 +0300

    created colors sizes cartesian product

--- 06_advanced-python-concepts/06_06_shirt_shop.py
+++ 06_advanced-python-concepts/06_06_shirt_shop.py
@@ -3,3 +3,6 @@
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+# [ (i,j) for i in range(1,3) for j in range(1,5) ]
+colors_sizes = [(color,size) for color in colors for size in sizes]
+print(colors_sizes)
