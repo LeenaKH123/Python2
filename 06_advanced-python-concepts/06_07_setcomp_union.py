2dd6f1e960aa2f5c2712aa0aa2899a678fd14dd4
root <root@DESKTOP-G07NF43.localdomain>
2021-09-14 12:21:29 +0300

    union set created :-)

--- 06_advanced-python-concepts/06_07_setcomp_union.py
+++ 06_advanced-python-concepts/06_07_setcomp_union.py
@@ -10,13 +10,26 @@
 
 s = {1, 2, 3, 4}
 t = {2, 3, 4, 5, 7}
-union_list = {}
-for itemS in s:
-    for itemT in t:
-        if itemS>2 and itemT>2:
-            union_list = (s|t)
+# ---working code 1 ----
+# union_set = set()
+# for itemS in s:
+#     for itemT in t:
+#         if itemS > 2:            
+#             union_set.add(itemS)
+#         if itemT > 2:
+#             union_set.add(itemT)
+# union_set_squared = [number**2 for number in union_set]
+# print(union_set_squared)
+#-----end of working code 1------
+#working code 2
+# union_set = s | t
+# union_set2 = set()
+# for x in union_set:
+#     if x>2:
+#         union_set2.add(x)
+# union_set_squared = [number**2 for number in union_set2]
+# print(union_set_squared)
+# ---end of working code 2 ---
 
-union_list = [number**2 for number in union_list]
-print(union_list)
-# union_list = [itemS**2 | itemT**2 for itemS in s for itemT in t if itemS > 2 if itemT > 2 ]
-# print(union_list)
+union_set = [number**2 for number in s|t if number > 2 ]
+print(union_set)
