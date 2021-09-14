ec404c41f73159a92e5bf8d15349b2823068e0e0
root <root@DESKTOP-G07NF43.localdomain>
2021-09-14 12:54:42 +0300

    generator

--- 06_advanced-python-concepts/06_09_generators.py
+++ 06_advanced-python-concepts/06_09_generators.py
@@ -1,3 +1,7 @@
 # Demonstrate how to create a generator object.
 # Print the object to the console to see what you get.
 # Then iterate over the generator object and print out each item.
+generator = (x*2 for x in range(5))
+print(generator)
+for x in generator:
+    print(x)
