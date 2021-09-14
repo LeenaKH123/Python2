8c92d3ca043e0d16a23de856bf57403d6e914687
root <root@DESKTOP-G07NF43.localdomain>
2021-09-14 13:02:20 +0300

    floored generator

--- 06_advanced-python-concepts/06_11_floored_generator.py
+++ 06_advanced-python-concepts/06_11_floored_generator.py
@@ -1,3 +1,7 @@
 # Adapt your Generator expression from the previous exercise:
 # Don't print anything, and run a floor division by 1111 on it.
 # What numbers do you get?
+nums = range(1, 1000000)
+generator = (x for x in nums)
+for x in generator:
+    print(x//1111)
